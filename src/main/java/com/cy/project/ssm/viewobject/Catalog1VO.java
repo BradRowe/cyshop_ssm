7d97de09376eac6dbca0c8c636e55adb314ec28d
wutong <602232262@qq.com>
2019-11-07 16:35:59 +0800

    字符编码过滤器

---
+++ src/main/java/com/cy/project/ssm/viewobject/Catalog1VO.java
@@ -0,0 +1,11 @@
+package com.cy.project.ssm.viewobject;
+
+/**
+ * @version 1.0.0
+ * @ClassName Catalog1VO
+ * @Description TODO
+ * @Author Administrator
+ * @date 2019/11/716:20
+ */
+public class Catalog1VO {
+}
---
+++ src/main/java/com/cy/project/ssm/viewobject/Catalog2VO.java
@@ -0,0 +1,13 @@
+package com.cy.project.ssm.viewobject;
+
+/**
+ * @version 1.0.0
+ * @ClassName Catalog2VO
+ * @Description TODO
+ * @Author Administrator
+ * @date 2019/11/716:23
+ */
+public class Catalog2VO {
+    private Integer id;
+    private String name;
+}
